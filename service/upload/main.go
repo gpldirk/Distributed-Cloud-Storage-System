90f3e98d04fa145c80eac2accab450b86b20b177
gpldirk <gepeilu@usc.edu>
2020-07-07 16:35:59 -0700

    5th commit

--- service/upload/main.go
+++ service/upload/main.go
@@ -3,46 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/cloud/config"
-	"github.com/cloud/handler"
-	"log"
-	"net/http"
+	"github.com/cloud/route"
 )
 
 func main() {
-	// 引入静态资源
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("./static"))))
-
-	// 文件路由接口设置
-	http.HandleFunc("/file/upload", handler.HTTPInterceptor(handler.UploadHandler))
-	http.HandleFunc("/file/upload/suc", handler.HTTPInterceptor(handler.UploadSucHandler))
-	http.HandleFunc("/file/meta", handler.HTTPInterceptor(handler.GetFileMetaHandler))
-	http.HandleFunc("/file/query", handler.HTTPInterceptor(handler.FileQueryHandler))
-	http.HandleFunc("/file/update", handler.HTTPInterceptor(handler.UpdateFileMetaHandler))
-	http.HandleFunc("/file/delete", handler.HTTPInterceptor(handler.DeleteFileHandler))
-	http.HandleFunc("/file/download", handler.HTTPInterceptor(handler.DownloadHandler))
-	http.HandleFunc("/file/downloadurl", handler.HTTPInterceptor(handler.DownloadURLHandler))
-	http.HandleFunc("/file/download/range", handler.HTTPInterceptor(handler.RangeDownloadHandler))
-
-	// 秒传接口
-	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler))
-
-	// 分块上传接口
-	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
-	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))
-	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))
-	http.HandleFunc("/file/mpupload/cancel", handler.HTTPInterceptor(handler.CancelUploadHandler))
-
-	// 用户接口
-	http.HandleFunc("/user/signup", handler.SignUpHandler)
-	http.HandleFunc("/user/signin", handler.SignInHandler)
-	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
-
+	r := route.Router()
+	r.Run(config.UploadServiceHost)
 	// 监听端口
 	fmt.Printf("上传服务已启动，监听： %s\n", config.UploadServiceHost)
-	err := http.ListenAndServe(config.UploadServiceHost, nil)
-	if err != nil {
-		log.Println(err.Error())
-	}
 }
