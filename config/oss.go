7eacb81bf3812ef7f5a2a82ca2f52157f0ffffd5
Gepei Lu <gepeilu@usc.edu>
2021-07-06 10:41:29 -0700

    Update oss.go

--- config/oss.go
+++ config/oss.go
@@ -1,9 +1,9 @@
 package config
 
 const (
-	OSSAccessKey = "LTAI4G8FyHb4n2trrZVwfwBU"
-	OSSSecretkey = "O2J6LnFPZfKP1QsV3P5JUqQW10P8Pe"
-	OSSEndPoint = "oss-us-west-1.aliyuncs.com"
+	OSSAccessKey = ""
+	OSSSecretkey = ""
+	OSSEndPoint = ""
 	OSSBucket = "bucket-file-store"
 )
 
